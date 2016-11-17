18505859bfbebab8540e536191b401bbab54ac8c
kfrn <knfrances@gmail.com>
2016-11-18 12:38:28 +1300

    Adding game instructions

--- templates/gameTemplate.js
+++ templates/gameTemplate.js
@@ -8,5 +8,14 @@ function gameTemplate (state, dispatch) {
 }
 
 function startButton () {
-  return h('button', {onclick: () => dispatch({type: action})})
+  return h('button', {onclick: () => dispatch({type: action})}, 'Push to start the fun')
+
+  let instructions = `
+  This game is a test of your shape knowledge
+  A shape will flash on the screen and the first player to correctly identify the shape and press their key gains a point.
+  Player 1 uses key (Q)
+  Player 2 uses key (P)
+  Press the start button to begin.
+  
+  `
 }
