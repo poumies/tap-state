c108615ce5cfc7dbdf9af7abd872f5ad9f0f95fa
Mel <mel.m.booth@gmail.com>
2016-11-18 13:29:07 +1300

    create initialState and updateView

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const morph = require('morphdom')
 const redux = require('redux')
 
 const reducer = require('./reducer')
+const shapes - require('./shapes')
 
 const initialState = {
-  
+  players: [
+    {id: 1, score: 0},
+    {id: 2, score: 0}
+  ],
+  images: [
+    shapes
+  ]
 }
 
 const store = redux.createStore(reducer, initialState)
 
 const dispatch = store.dispatch
+
+const updateView = () => {
+  const initialState = store.getState()
+  const newState = render(initialState.shapes, dispatch)
+  morph(products, newState)
+}
